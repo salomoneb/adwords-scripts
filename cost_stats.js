e07df64d8e8b9dd0e3bc8b5b6933b9dd75cde77e
Salomone Baquis <salomone.l.baquis@gmail.com>
2016-07-12 23:30:46 -0400

    initial commit

---
+++ cost_stats.js
@@ -0,0 +1,40 @@
+function main() {
+  
+  // Global vars
+  var date = new Date()
+  var currentDate = Utilities.formatDate(date, "EST", "yyyyMMdd")
+  var cellDate = Utilities.formatDate(date, "EST", "MM/dd/yyyy")
+  var campaignCondition = "Name='PASTE YOUR CAMPAIGN NAME HERE'"
+  
+  var spreadsheetUrl = "PASTE YOUR SPREADSHEET URL LINK HERE"
+  var sheetName = "PASTE THE SPREADSHEET TAB NAME HERE"
+  
+  // Invoked function to copy everything to the spreadsheet.
+  copyData()
+  
+  // Gets campaign cost data 
+  function getData() {
+    var campaignIterator = AdWordsApp.campaigns().withCondition(campaignCondition).get()
+    
+    while (campaignIterator.hasNext()) {
+      var campaign = campaignIterator.next()
+      var campaignStart = campaign.getStartDate()    // Unlike the current date, this returns an object.
+      var costRange = campaign.getStatsFor(campaignStart, currentDate)
+      var cost = costRange.getCost()
+    }
+    return cost
+  }
+  
+  function copyData() {
+    var ss = SpreadsheetApp.openByUrl(spreadsheetUrl)
+    var sheet = ss.getSheetByName(sheetName)
+    
+    // Selects spreadsheet cells
+    var dateCell = sheet.getRange("PASTE CELL1 HERE (EX. A2) : PASTE CELL1 HERE")
+    var costCell = sheet.getRange("PASTE CELL2 HERE (EX. B4) : PASTE CELL2 HERE")
+    
+    // Sets cell values
+    dateCell.setValue(cellDate)
+    costCell.setValue(getData())    // References data returned in getData function.
+  } 
+}
